4e4d741dc7d6fb8dc48274203c468e7428930328
Jake Lawrence <Jake Lawrence>
2021-03-06 12:21:44 +0000

    Create placeholder base.html  - Add templates folder and register with settings.py  - Add static folder and register with settings.py  - Updated test view to render base.html  - Temporarily commented out models so than server can run

--- wrkout/models.py
+++ wrkout/models.py
@@ -1,40 +1,40 @@
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-class User(models.Model):
-    UserID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
-    SavedWorkouts = models.ManyToManyField(Workout)
-    LikedWorkouts = models.ManyToManyField(Workout)
-    LikedExercises = models.ManyToManyField(Exercises)
-    Username = models.CharField(max_length=30)
-    Password = models.CharField(max_length=30)
-    ProfilePicture = models.ImageField(upload_to='profile_images', blank=True)
+# class User(models.Model):
+#     UserID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
+#     SavedWorkouts = models.ManyToManyField(Workout)
+#     LikedWorkouts = models.ManyToManyField(Workout)
+#     LikedExercises = models.ManyToManyField(Exercises)
+#     Username = models.CharField(max_length=30)
+#     Password = models.CharField(max_length=30)
+#     ProfilePicture = models.ImageField(upload_to='profile_images', blank=True)
 
-class Exercise(models.Model):
-    ExerciseID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
-    CreatorID = models.ForeignKey(User)
-    Name = models.CharField(max_length=30)
-    Description = models.CharField(max_length=500)
-    Difficulty = models.IntegerRangeField(min_value=1, max_value=5)
-    DemoImage = models.ImageField(upload_to='exercise_images', blank=True)
-    DemoVideo = models.URLField(blank=True)
-    Date = DateField()
-    Likes = IntegerField(default=0)
+# class Exercise(models.Model):
+#     ExerciseID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
+#     CreatorID = models.ForeignKey(User)
+#     Name = models.CharField(max_length=30)
+#     Description = models.CharField(max_length=500)
+#     Difficulty = models.IntegerRangeField(min_value=1, max_value=5)
+#     DemoImage = models.ImageField(upload_to='exercise_images', blank=True)
+#     DemoVideo = models.URLField(blank=True)
+#     Date = DateField()
+#     Likes = IntegerField(default=0)
     
     
 
-class Workout(models.Model):
-    WorkoutID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
-    CreatorID = models.ForeignKey(User)
-    Sets = models.ManyToManyField(Set)
-    Name = models.CharField(max_length=30)
-    Description = models.CharField(max_length=500)
-    Date = DateField()
-    Likes = IntegerField(default=0)
+# class Workout(models.Model):
+#     WorkoutID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
+#     CreatorID = models.ForeignKey(User)
+#     Sets = models.ManyToManyField(Set)
+#     Name = models.CharField(max_length=30)
+#     Description = models.CharField(max_length=500)
+#     Date = DateField()
+#     Likes = IntegerField(default=0)
 
-class Set(models.Model):
-    SetID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
-    ExerciseID = models.ForeignKey(Exercise)
-    NoOfReps = IntegerField(min_value=1)
+# class Set(models.Model):
+#     SetID = models.AutoField(primary_key=True, on_delete=models.CASCADE)
+#     ExerciseID = models.ForeignKey(Exercise)
+#     NoOfReps = IntegerField(min_value=1)
     
     
--- wrkout/views.py
+++ wrkout/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def test_view(request):
-    return HttpResponse("test!")
+    return render(request, 'wrkout/base.html')
