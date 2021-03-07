1d443a4e204e619a58dd19a62080a0f7a54d8834
Jake Lawrence <Jake Lawrence>
2021-03-07 15:17:45 +0000

    Sidebar

--- wrkout/urls.py
+++ wrkout/urls.py
@@ -3,6 +3,9 @@
 from django.urls import path
 from wrkout import views
 
+app_name = 'wrkout'
+
 urlpatterns = [
     path('test/', views.test_view, name='test'),
+    path('test2/', views.test_view, name='test2'),
 ]
