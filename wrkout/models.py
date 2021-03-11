d5afaa21fadbfde7e9d3049195ea5ddb178e83fc
Lewis Ponsonby <2464980P@student.gla.ac.uk>
2021-03-11 11:49:00 +0000

    Converted users to built in package and added slug fields to models

--- wrkout/models.py
+++ wrkout/models.py
@@ -2,18 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class User(models.Model):
+class UserProfile(models.Model):
+    UserAccount = models.OneToOneField(User, on_delete=models.CASCADE)
+    
     UserID = models.AutoField(primary_key=True)
+    Slug = models.SlugField(unique=True, null=True)
     SavedWorkouts = models.ManyToManyField('Workout', related_name='%(class)sSaved')
     LikedWorkouts = models.ManyToManyField('Workout')
     LikedExercises = models.ManyToManyField('Exercise')
-    Username = models.CharField(max_length=30)
-    Password = models.CharField(max_length=30)
     ProfilePicture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.UserProfile.username
+
+    def save(self, *args, **kwargs):
+        self.Slug = slugify(self.UserAccount.username)
+        super(UserProfile, self).save(*args, **kwargs)
 
 class Exercise(models.Model):
     ExerciseID = models.AutoField(primary_key=True)
-    CreatorID = models.ForeignKey(User, on_delete=models.CASCADE)
+    CreatorID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    Slug = models.SlugField(unique=True, null=True)
     Name = models.CharField(max_length=30)
     Description = models.CharField(max_length=500)
     Difficulty = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
@@ -21,17 +30,33 @@ class Exercise(models.Model):
     DemoVideo = models.URLField(blank=True)
     Date = models.DateField()
     Likes = models.IntegerField(default=0)
+
+    def Save(self, *args, **kwargs):
+        self.Slug = slugify(self.Name)
+        super(Exercise, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.Name
     
     
 
 class Workout(models.Model):
     WorkoutID = models.AutoField(primary_key=True)
-    CreatorID = models.ForeignKey(User, on_delete=models.CASCADE)
+    CreatorID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    Slug = models.SlugField(unique=True, null=True)
     Sets = models.ManyToManyField('Set')
     Name = models.CharField(max_length=30)
     Description = models.CharField(max_length=500)
+    Difficulty = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
     Date = models.DateField()
     Likes = models.IntegerField(default=0)
+
+    def Save(self, *args, **kwargs):
+        self.Slug = slugify(self.Name)
+        super(Workout, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.Name
 
 class Set(models.Model):
     SetID = models.AutoField(primary_key=True)
