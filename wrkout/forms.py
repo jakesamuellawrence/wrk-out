aa07623665fc391668d93be54461b471c1ec50a1
Minh Duc Pham <2439753P@student.gla.ac.uk>
2021-03-12 16:32:38 +0100

    user form

--- wrkout/forms.py
+++ wrkout/forms.py
@@ -21,3 +21,10 @@ class SetForm(forms.ModelForm):
     class Meta:
         model = Set
         fields = ('ExerciseID', 'No. of reps', )
+        
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
