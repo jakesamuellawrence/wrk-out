2fa73c0849fe5e101ef8e6d7a5bf7462566c97bd
Jake Lawrence <Jake Lawrence>
2021-03-12 15:08:56 +0000

    Change "exercises" to "results" in views

--- wrkout/views.py
+++ wrkout/views.py
@@ -45,16 +45,16 @@ def show_workout(request, category_name_slug):
     return render(request, 'wrkout/workout.html', context_dict['workout'])
     
 def browse_workouts(request):
-    workout_list = Workout.objects.order_by('-likes')
+    workout_list = Workout.objects.order_by('-Likes')
     context_dict = {}
-    context_dict['workouts'] = workout_list
+    context_dict['results'] = workout_list
     response = render(request, 'wrkout/browse.html', context=context_dict)
     return response
     
 def browse_exercises(request):
-    exercise_list = Exercise.objects.order_by('-likes')
+    exercise_list = Exercise.objects.order_by('-Likes')
     context_dict = {}
-    context_dict['exercises'] = exercise_list
+    context_dict['results'] = exercise_list
     response = render(request, 'wrkout/browse.html', context=context_dict)
     return response
     
