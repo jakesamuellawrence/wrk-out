c41b722c4a53237e5d375b62c9f0e3ad0618b75d
Minh Duc Pham <2439753P@student.gla.ac.uk>
2021-03-12 10:07:55 +0100

    population script and some forms

--- wrkout/models.py
+++ wrkout/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.template.defaultfilters import slugify
 
 class UserProfile(models.Model):
     UserAccount = models.OneToOneField(User, on_delete=models.CASCADE)
