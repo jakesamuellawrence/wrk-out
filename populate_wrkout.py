11e3980c7f1d9f983fb9a978b2232b4dd96459c7
Lewis Ponsonby <2464980P@student.gla.ac.uk>
2021-03-16 16:01:07 +0000

    Added accounts, exercises and sets to population script. Clear database before running it for the first time.

--- populate_wrkout.py
+++ populate_wrkout.py
@@ -5,79 +5,78 @@ import django
 django.setup()
 
 from wrkout.models import Exercise, Workout, UserProfile, Set
+from django.contrib.auth.models import User
+from datetime import datetime
 
 def populate():
-    
-    exercises = [] #dictionaries containing exercises go here
-    #format is something like {'CreatorID' = CreatorID, 'name' = name,
-    #                       'difficulty' = difficulty, 'description' = description, 'date' = date, 'likes' = likes}
-    
-    workouts = [] #dictionaries containing workouts go here
-     
-    #format is something like exercises, but it also key 'sets' = {reps:exercise1, reps:exercise2, etc...}
-        #                             (will later on create a list of sets from this dictionary)
-    
-    userprofiles = [] #dictionaries containing user profiles go here
-    #format is something like {'SavedWorkouts' = SW, 'LikedWorkouts' = LW, 'LikedExercises' = LE}
-    
-    
-    for ex in exercises:
-        CID = ex['CreatorID']
-        name = ex['name']
-        difficulty = ex['difficulty']
-        description = ex['description']
-        date = ex['date']
-        likes = ex['likes']
-        
-        add_exercise(CID, name, difficulty, description, date, likes)
-        
-    for w in workouts:
-        
-        sets = [] #list of sets...? not sure how many to many field works
-        for reps, exercise in w['sets']:
-            ExID = exercise.ExerciseID
-            s = Set(ExerciseID = ExID, Reps = reps)
-            sets.append(s)
-        
-        CID = w['CreatorID']
-        name = w['name']
-        difficulty = w['difficulty']
-        description = w['description']
-        date = w['date']
-        likes = w['likes']                       
-        
-        add_workout(CID, sets, name, description, difficulty, date, likes)
-        
-    for user in userprofiles:
-        
-        SW = user['SavedWorkouts']
-        LW = user['LikedWorkouts']
-        LE = user['LikedExercises']
-        
-        add_userprofile(SW, LW, LE)
-        
 
-def add_exercise(CID, name, difficulty, description, date, likes):
-    e = Exercise.get_or_create(CreatorID = CID, Name = name, Difficulty = difficulty,
-                               Description = description,Date = date, Likes = likes)
-    e.save()
-    return e
+    useraccounts = [{'username':'testname',
+                     'password':'testpassword',
+                     'email':'testemail@gmail.com'},]
 
+    
+    
+    exercises = [{'name':'testname', 
+                  'description':'test description',
+                  'difficulty':3,
+                  'date':'2021-03-16',
+                  'likes':12,
+                  'creatorID':16},]
 
-def add_workout(CID, sets, name, description, difficulty, date, likes):
-    w = Workout.get_or_create(CreatorID = CID, Sets = sets, Name = name,
-                              Description = description, Difficulty = difficulty, 
-                              Date = date, Likes = likes)
-    w.save()
-    return w
+    sets=[{'exercisename':'testname', #SetID = 1
+           'noofreps':'12'},
+          {'exercisename':'testname', #SetID = 2
+           'noofreps':'6'},]
+    
+    workouts = [] 
+    
+    userprofiles = [] 
+
+    for user in useraccounts:
+        add_user(user['username'],user['password'],user['email'])
+
+    for exercise in exercises:
+        add_exercise(exercise['name'],exercise['description'],exercise['difficulty'],exercise['date'],exercise['likes'],exercise['creatorID'])
+
+    for a_set in sets:
+        add_set(a_set['exercisename'],a_set['noofreps'])
     
     
 
-def add_userprofile(SW, LW, LE):
-    user = UserProfile.get_or_create(SavedWorkouts = SW, LikedWorkouts = LW, LikedExercises = LE)
-    user.save()
+        
+
+def add_user(username, password, email):
+    try:
+        useraccount = User.objects.create_user(username, password)
+        userprofile=UserProfile.objects.get_or_create(UserAccount=useraccount,UserID=useraccount.id)[0]
+    except:
+        useraccount = User.objects.get(username=username, password=password)
+        userprofile=UserProfile.objects.get_or_create(UserAccount=useraccount,UserID=useraccount.id)[0]
+    useraccount.username=username
+    useraccount.password=password
+    useraccount.email=email
+    useraccount.save()
+    return useraccount
+
+
+def add_exercise(name, description, difficulty, date=datetime.now(), likes=0, creatorID=1):
+    creator=UserProfile.objects.get(UserID=creatorID)
+    exercise = Exercise.objects.get_or_create(Name=name, Description=description, Difficulty=difficulty, Date=datetime.now(), Likes=likes, CreatorID=creator)[0]
+    exercise.Name=name
+    exercise.Description=description
+    exercise.Difficulty=difficulty
+    exercise.Date=date
+    exercise.Likes=likes
+    exercise.CreatorID=creator
+    exercise.save()
+    return exercise
+
+def add_set(exercisename, noofreps):
+    exerciseid=Exercise.objects.get(Name=exercisename)
+    a_set = Set.objects.get_or_create(ExerciseID=exerciseid, NoOfReps=noofreps)
     
-    return user
+    
+
     
 
  
