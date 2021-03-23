a43eaf986f399387cbf6a7596ecd5675ffd3a8e7
Jake Lawrence <Jake Lawrence>
2021-03-23 16:54:20 +0000

    Make links to exercises and workouts work

--- wrkout/urls.py
+++ wrkout/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('exercises/browse/popular/', views.browse_popular_exercises, name='exercises_popular'),
     path('exercises/browse/new/', views.browse_newest_exercises, name='exercises_new'),
     path('exercises/create/', views.create_exercise, name='create_exercise'),
-    path('exercises/<slug:exercise_Name_Slug>/', views.show_exercise, name='show_exercises')
+    path('exercises/<slug:exercise_Name_Slug>/', views.show_exercise, name='show_exercise')
     
     #path('users/', views.show_profile, name='show'),
     #path('users/edit/', views.edit_profile, name='edit'),
