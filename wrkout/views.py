34dd464c6cb246e69d3b7d9ea94dffd6cade308d
Martina Karaskova <2478724k@student.gla.ac.uk>
2021-03-26 15:32:27 +0000

    browse by most difficult

--- wrkout/views.py
+++ wrkout/views.py
@@ -86,6 +86,20 @@ def browse_newest_exercises(request):
     context_dict['results'] = exercise_list
     response = render(request, 'wrkout/browse.html', context=context_dict)
     return response
+    
+def browse_difficulty_workouts(request):
+    workout_list = Workout.objects.order_by('-Difficulty')
+    context_dict = {}
+    context_dict['results'] = workout_list
+    response = render(request, 'wrkout/browse.html', context=context_dict)
+    return response
+    
+def browse_difficulty_exercises(request):
+    exercise_list = Exercise.objects.order_by('-Difficulty')
+    context_dict = {}
+    context_dict['results'] = exercise_list
+    response = render(request, 'wrkout/browse.html', context=context_dict)
+    return response
 
 def view_profile(request,username):
     try:
