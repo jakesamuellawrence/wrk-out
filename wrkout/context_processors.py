5b1ead4435f90d149dcbd47e4ad28431da504dc6
Jake Lawrence <Jake Lawrence>
2021-03-30 10:13:34 +0100

    fixed superuser issue

--- wrkout/context_processors.py
+++ wrkout/context_processors.py
@@ -3,9 +3,8 @@ from wrkout.models import UserProfile
 def user_profiles(request):
     if request.user.is_authenticated == False: 
         return {}
-    try:
-        userprof = UserProfile.objects.get(UserAccount=request.user)
-    except UserProfile.DoesNotExist:
-        return {}
+
+    # This will automatically create a profile for any user that doesn't have one (e.g. admins)
+    userprof = UserProfile.objects.get_or_create(UserAccount=request.user)[0]
     
     return {'logged_in_profile': userprof}
